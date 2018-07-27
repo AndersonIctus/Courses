48c1a3b741487ef503054ae6886a5498104a2072
Anderson Dourado <anderson.ictus@gmail.com>
2018-07-26 22:00:16 -0300

    * AULA de Asp.NET MVC 5 Framework: ------------------------------------------ * Dados das Aulas 1 a 4   - Conhecendo o Asp.Net MVC 5   - Vendo um pouco sobreo padrão MVC em si   - Fazendo um Controller para listar Produtos e Categorias   - Fazendo um Controller para Cadastrar novos Produtos e Categorias

---
+++ APS_NET_MVC5/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,38 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Categorias = new CategoriasDAO().Lista();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+
+            return RedirectToAction("Index", "Produto");
+        }
+    }
+}
